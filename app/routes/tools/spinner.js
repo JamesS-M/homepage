bb9d9bb963a19f6b5bb91914f2c2a82d9ef4919e
James Summerby-Murray <jsummerb@gmail.com>
2019-11-11 17:51:24 -0400

    feat: Spinner receives data from route

--- app/routes/tools/spinner.js
+++ app/routes/tools/spinner.js
@@ -1,3 +1,10 @@
 import Route from '@ember/routing/route'
 
-export default class SpinnerRoute extends Route {}
+export default class SpinnerRoute extends Route {
+
+  model() {
+    return [
+      1, 2, 3, 4, 5, 6, 7, 8, 9
+    ]
+  }
+}
